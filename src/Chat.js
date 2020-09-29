3696982a1d530bd3c7d627eea9d481cd66fcebfe
Rooppesh <rooppesh98@gmail.com>
2020-09-30 02:36:23 +0530

    Basic UI

---
+++ src/Chat.js
@@ -0,0 +1,59 @@
+import React from "react";
+import "./Chat.css";
+import { Avatar, IconButton } from "@material-ui/core";
+import {
+  SearchOutlined,
+  AttachFile,
+  MoreVert,
+  InsertEmoticon,
+  Mic,
+} from "@material-ui/icons";
+function Chat() {
+  return (
+    <div className="chat">
+      <div className="chat__header">
+        <Avatar />
+        <div className="chat__headerInfo">
+          <h3>Room Name</h3>
+          <p>Description</p>
+        </div>
+
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className="chat__body">
+        <p className="chat__message">
+          <span className="chat__name">Roop</span>
+          Message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+        <p className="chat__message chat__reciever">
+          <span className="chat__name">Roop</span>
+          Message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+      </div>
+
+      <div className="chat__footer">
+        <InsertEmoticon />
+        <form>
+          <input placeholder="Type a Message" type="text" />
+          <button type="submit">Send a Message</button>
+        </form>
+        <Mic />
+      </div>
+    </div>
+  );
+}
+
+export default Chat;
---
+++ src/Sidebar.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "./Sidebar.css";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertical from "@material-ui/icons/MoreVert";
+import { Avatar, IconButton } from "@material-ui/core";
+import { SearchOutlined } from "@material-ui/icons";
+import SidebarChat from "./SidebarChat";
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <div className="sidebar__headerLeft">
+          <Avatar src=""></Avatar>
+        </div>
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertical />
+          </IconButton>
+        </div>
+      </div>
+      <div className="sidebar_search">
+        <div className="sidebar__searchContainer">
+          <SearchOutlined />
+          <input placeholder="Search or Start New Chat" type="text" />
+        </div>
+      </div>
+      <div className="sidebar__chats">
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
